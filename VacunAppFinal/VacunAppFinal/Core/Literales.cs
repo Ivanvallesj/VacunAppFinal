ed20f32395d38f549cd30dc2e629271d3fa55018
Ramiro Córdoba <ramicordoba2001@gmail.com>
2021-12-14 23:19:00 -0300

    Estética

--- VacunAppFinal/VacunAppFinal/Core/Literales.cs
+++ VacunAppFinal/VacunAppFinal/Core/Literales.cs
@@ -8,14 +8,14 @@ namespace VacunAppFinal.Core
     {
         public static string TituloApp = "VacunApp";
         //*****Pantalla Registrar Nueva cuenta
-        public static string TituloMiCuenta = "Mi cuenta";
+        public static string TituloMiCuenta = "Mi Cuenta";
         public static string LabelNombre = "Nombre:";
         public static string LabelApellido = "Apellido:";
         public static string LabelEmail = "Email:";
         public static string LabelBienvenido = "Bienvenido ";
         public static string LabelContraseña = "Contraseña:";
         //*****Pantalla Iniciar sesión
-        public static string TituloIniciarSesion = "Iniciar sesión";
+        public static string TituloIniciarSesion = "Iniciar Sesión";
         public static string TituloOlvidasteTuContraseña = "¿Olvidaste tu contraseña?";
 
         public static string TituloBotonNuevo = "Nuevo";
@@ -23,12 +23,12 @@ namespace VacunAppFinal.Core
         public static string TituloBotonEditar = "Editar";
         public static string TituloBotonEliminar = "Borrar";
         public static string TituloBotonCrearCuenta = "Crear cuenta";
-        public static string TituloBotonConectarFacebook = "Conectar con facebook";
-        public static string TituloBotonConectarGoogle = "Conectar con google";
+        public static string TituloBotonConectarFacebook = "Conectar con Facebook";
+        public static string TituloBotonConectarGoogle = "Conectar con Google";
         public static string TituloBotonSalir = "Salir";
         public static string TituloBotonRegistrarme = "Registrarme";
-        public static string TituloBotonRegistrarHijos = "Registrar hijos";
-        public static string TituloBotonRegistrarVacunación= "Registrar vacunación";
+        public static string TituloBotonRegistrarHijos = "Registrar Hijos";
+        public static string TituloBotonRegistrarVacunación= "Registrar Vacunación";
         public static string TituloBotonIniciarSesion = "Iniciar Sesión";
         public static string TituloBotonTabbedPage = "Página de pestañas";
         public static string TituloTabbedPageVacunas = "Vacunas en pestañas";
